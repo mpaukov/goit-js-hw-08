a6d31d919a0cf5fdb4520f986f0b1d704925f2c8
Michael Paukov <michael.paukov@gmail.com>
2021-12-30 20:17:46 +0100

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const makeGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+    <a class="gallery__item" href="${original}">
+    <img 
+    style="display:block"
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+  </a>
+  `;
+};
+
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+
+const galleryElements = document.querySelector('.gallery');
+
+galleryElements.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
