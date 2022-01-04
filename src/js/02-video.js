68b5a37db300a05a1ee763da02d6f46c762d545c
Michael Paukov <michael.paukov@gmail.com>
2022-01-04 16:23:41 +0100

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const onPlay = function (data) {
+  const key = localStorage.getItem('videoplayer-current-time');
+  if (key) {
+    player.setCurrentTime(parseFloat(key));
+  }
+  player.on('timeupdate', throttle(timeUpdate, 1000));
+  player.off('play', onPlay);
+};
+
+function timeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds.toString());
+}
+
+player.on('play', onPlay);
