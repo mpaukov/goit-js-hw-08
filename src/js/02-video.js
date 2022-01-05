795d6ca2577b9f2d7531ee9d30674e898d30fd53
Michael Paukov <michael.paukov@gmail.com>
2022-01-05 15:24:29 +0100

    task3 after review

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const KEY = 'videoplayer-current-time';
+
 const player = new Player('vimeo-player');
-
-const onPlay = function (data) {
-  const key = localStorage.getItem('videoplayer-current-time');
-  if (key) {
-    player.setCurrentTime(parseFloat(key));
-  }
-  player.on('timeupdate', throttle(timeUpdate, 1000));
-  player.off('play', onPlay);
-};
-
-function timeUpdate(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds.toString());
+const key = localStorage.getItem(KEY);
+if (key) {
+  player.setCurrentTime(parseFloat(key));
 }
 
-player.on('play', onPlay);
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(KEY, data.seconds.toString());
+  }, 1000),
+);
